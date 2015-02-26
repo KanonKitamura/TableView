fc3505165cedd5659e2d20008341706955a135fc
KanonKitamura <kanonkitamura0405@gmail.com>
2015-02-26 21:13:36 +0900

    Initial Commit

---
+++ album/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  album
+//
+//  Created by kanon kitamura on 2015/02/26.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ album/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  album
+//
+//  Created by kanon kitamura on 2015/02/26.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
